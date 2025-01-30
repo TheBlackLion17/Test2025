466c54894ea01abaf7cccbfaa7fae9381456415c
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-01-30 22:30:41 +0530

    V0.01

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TheBlackLion17/AADVAN.git /AADVAN
+  git clone https://github.com/TheBlackLion17/Test2025.git /Test2025
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AADVAN
+  git clone $UPSTREAM_REPO /Test2025
 fi
-cd /AADVAN
+cd /Test2025
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
